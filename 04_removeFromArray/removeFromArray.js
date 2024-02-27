271f9baa55845834a7bc9cf78bde9cfcf9f17cc3
Christian Anderson <christian.anderson982@gmail.com>
2024-02-26 22:50:54 -0500

    Solved. Need to solve in a more JS way

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,25 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    let retArr = [];
+    let inclusionFlag = true;
 
+    for(let i = 0; i < arr.length; ++i)
+    {
+        for(let j = 0; j < args.length; ++j)
+        {
+            if(arr[i] === args[j])
+            {
+                inclusionFlag = false;
+                break;
+            }
+        }
+        if(inclusionFlag)
+        {
+            retArr.push(arr[i]);
+        }
+        inclusionFlag = true;
+    }
+
+    return retArr;
 };
 
 // Do not edit below this line
