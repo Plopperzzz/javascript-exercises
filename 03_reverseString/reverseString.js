5672d2c52baf9e7e84173268a98311323dba4387
Christian Anderson <christian.anderson982@gmail.com>
2024-02-26 22:23:10 -0500

    Cleared

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let retStr = '';
+    
+    for(let i = str.length - 1; i >=0; --i)
+    {
+        retStr += str[i];
+    }
+    
+    return retStr;
 };
 
 // Do not edit below this line
