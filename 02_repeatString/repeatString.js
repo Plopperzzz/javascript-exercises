3da33e6f472340a812493147cf765a1d9db64725
Christian Anderson <christian.anderson982@gmail.com>
2024-02-26 22:18:41 -0500

    Excersizes

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
 
+    if(num < 0){
+        return 'ERROR';
+    }
+    let tempStr = str;
+    let retStr = '';
+
+    for(let i = 0; i < num; ++i)
+    {
+        retStr += tempStr;
+    }
+    return retStr;
 };
 
 // Do not edit below this line
