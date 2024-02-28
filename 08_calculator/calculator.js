446b516cd9cf03a58cdd73ee40cb9205eea6256c
Christian Anderson <christian.anderson982@gmail.com>
2024-02-27 19:43:50 -0500

    Solved.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,52 @@
-const add = function() {
-	
+const add = function(x, y) {
+  return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+  return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((a,b)=>a+b,0);
 };
 
-const multiply = function() {
+// logarithmic multiple without the * operator
+// wrong input type though...
+/*
+const multiply = function(a, b, acc=0) {
+  if(b == 1)
+  {
+    return a + acc;
+  }
+  if(a == 1)
+  {
+    return b + acc;
+  }
+  if(b%2 == 1)
+  {
+    return multiply(a + a, b >> 1, acc + a);
+  }
+  else{
+    return multiply(a + a, b>>1, acc);
+  }
+};
+*/
+const multiply = function(arr){
+  return arr.reduce((a,b) => a*b, 1);
+}
 
+const power = function(x, y, acc=1) {
+  return y == 1 ? x*acc : y%2==1 ? power(x * x, y >> 1, acc*x) : power(x*x, y>>1, acc);
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(num) {
+  let res = 1;
+  let limit = num;
+  for(let i = 0; i < limit; ++i)
+  {
+    res *= num--;
+  }
+  return res;
 };
 
 // Do not edit below this line
