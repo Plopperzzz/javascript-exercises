42505ae64dc095d79294a14c2958a92e27648d32
Christian Anderson <christian.anderson982@gmail.com>
2024-03-04 11:07:21 -0500

    Solved

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,11 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let regex = /[a-zA-Z0-9-]+/g;
+    let newStr =  str.toUpperCase().match(regex).join('');
+    let revStr = newStr.split('').reverse().join(''); 
+
+    console.log(newStr);
+    console.log(revStr);
+    return newStr === revStr;
 
 };
 
