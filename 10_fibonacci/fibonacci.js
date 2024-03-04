3f2ed478a9965c26977edbbd57dc8e32b61d4ce9
Christian Anderson <christian.anderson982@gmail.com>
2024-03-03 23:09:06 -0500

    Solved

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(n, a=0, b=1) {
+    n = Number(n);
+    if(n < 0)
+    {
+        return 'OOPS';
+    }
+    if(n<=1)
+    {
+        return b - (n===0);
+    }
+    else{
+        return fibonacci(n-1,b, a+b);
+    }
 
 };
 
