28a10fb887d4fc9c693106ffbb4d1dbdc579f3d6
Christian Anderson <christian.anderson982@gmail.com>
2024-03-03 23:19:45 -0500

    Solved

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,5 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.sort((a,b)=>{return (a.yearOfBirth - (a.yearOfDeath === undefined ? (new Date()).getFullYear() : a.yearOfDeath)) >= (b.yearOfBirth - (b.yearOfDeath === undefined ? (new Date()).getFullYear() : b.yearOfDeath)) ? 1 : -1})[0];
 };
 
 // Do not edit below this line
